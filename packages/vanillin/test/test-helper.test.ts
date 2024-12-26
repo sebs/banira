eedc2dabf82c4971a5602de32a758b4bb559825e
Sebastian Schürmann <sebastian.schuermann@maibornwolff.de>
2024-12-26 22:40:25 +0100

    refactor(vanillin): load deps beforehand

--- packages/vanillin/test/test-helper.test.ts
+++ packages/vanillin/test/test-helper.test.ts
@@ -7,28 +7,14 @@ import { TestHelper, MountContext } from '../src/test-helper.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const myCircleJs = readFileSync(resolve(__dirname, './fixtures/my-circle.js'), 'utf8');
 
 describe('TestHelper', () => {
-    describe('JS moddule', () => {
-        var document: Document;
-        beforeEach(async () => {
-            const simpleJs = readFileSync(resolve(__dirname, './fixtures/simple.js'), 'utf8');
-            const helper = new TestHelper();
-            const buildResult = await helper.mountAsScript('script', simpleJs);
-            document = buildResult.document;
-        });
-
-        it('returns a document', async() => {
-            assert.ok(document);
-        });
-    });
-
-    describe('Simple web component', () => {
+    
         var buildResult: MountContext;
         beforeEach(async () => {
-            const simpleJs = readFileSync(resolve(__dirname, './fixtures/my-circle.js'), 'utf8');
             const helper = new TestHelper();
-            buildResult = await helper.mountAsScript('my-circle', simpleJs); 
+            buildResult = await helper.mountAsScript('my-circle', myCircleJs); 
         });
 
         afterEach(async () => {
@@ -47,6 +33,5 @@ describe('TestHelper', () => {
             assert.ok(circle, 'Circle element should exist');
             assert.strictEqual(circle?.tagName.toLowerCase(), 'my-circle');
         });
-    });
     
 });
