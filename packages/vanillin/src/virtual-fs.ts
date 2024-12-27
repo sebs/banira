6d8f3fed41ae66ebb9dabe077d02b012714bff3e
Sebastian Schürmann <sebastian.schuermann@maibornwolff.de>
2024-12-27 20:29:13 +0100

    refactor(vanillin): virtual-fs usage of typescript types improved

--- packages/vanillin/src/virtual-fs.ts
+++ packages/vanillin/src/virtual-fs.ts
@@ -1,7 +1,7 @@
 import { Dirent } from 'memfs/lib/Dirent';
 import { Dirent as FsDirent } from 'fs';
 import { Volume } from 'memfs';
-import * as ts from 'typescript';
+import { CompilerHost, ScriptTarget, createSourceFile } from 'typescript';
 import { dirname } from 'path';
 
 export interface VirtualFileSystemOptions {
@@ -9,7 +9,7 @@ export interface VirtualFileSystemOptions {
     cwd?: string
 }
 
-export interface VirtualCompilerHost extends ts.CompilerHost {
+export interface VirtualCompilerHost extends CompilerHost {
     volume: InstanceType<typeof Volume>;
 }
 
@@ -18,15 +18,15 @@ export function createVirtualCompilerHost(options: VirtualFileSystemOptions): Vi
 
     return {
         volume,
-        getSourceFile: (fileName: string, languageVersion: ts.ScriptTarget) => {
+        getSourceFile: (fileName: string, languageVersion: ScriptTarget) => {
             try {
                 const sourceText = volume.readFileSync(fileName, 'utf8');
-                return ts.createSourceFile(fileName, sourceText.toString(), languageVersion);
+                return createSourceFile(fileName, sourceText.toString(), languageVersion);
             } catch {
                 return undefined;
             }
         },
-        getDefaultLibFileName: options => options.target === ts.ScriptTarget.ES5 ? 'lib.d.ts' : 'lib.es6.d.ts',
+        getDefaultLibFileName: options => options.target === ScriptTarget.ES5 ? 'lib.d.ts' : 'lib.es6.d.ts',
         writeFile: (fileName: string, data: string) => {
             const dir = dirname(fileName);
             if (!volume.existsSync(dir)) {
