51a4cd6f7aaf8bb5e6db33ba0af8b2f03908c16f
Sebastian Schürmann <sebastian.schuermann@maibornwolff.de>
2025-01-04 00:32:46 +0100

    feature(banira): use picocss as basis

--- packages/banira/src/formatter/doc-page.ts
+++ packages/banira/src/formatter/doc-page.ts
@@ -27,6 +27,10 @@ export class FormatterDocPage {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
     <script type="module" src="${src}"></script>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    >
 </head>
 <body>
     <h1>${title}</h1>
